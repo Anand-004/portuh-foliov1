5686c58d567a67a1349edd29a934abb79b5da8b6
Anand-004 <anandsince2004@gmail.com>
2025-12-19 13:25:02 +0530

    finished about framework

--- index.js
+++ index.js
@@ -6,12 +6,55 @@ function check() {
 
 var prevScrollpos = window.pageYOffset;
 var overlay = document.getElementById("overlay");
+var skilltab = document.getElementById("skill");
+var educationtab = document.getElementById("education");
+var experiencetab = document.getElementById("experience");
+var toolstab = document.getElementById("tools");
+var qualitiestab = document.getElementById("qualities");
+var filestab = document.getElementById("files");
 
 function showoverlay() {
   overlay.style.display = "flex";
 }
 function hideoverlay() {
   overlay.style.display = "none";
+}
+function showskill() {
+  skilltab.style.display = "block";
+}
+function showeducation() {
+  educationtab.style.display = "block";
+}
+function showexperience() {
+  experiencetab.style.display = "block";
+}
+function showtools() {
+  toolstab.style.display = "block";
+}
+function showqualities() {
+  qualitiestab.style.display = "block";
+}
+function showfiles() {
+  filestab.style.display = "block";
+}
+
+function hideskills() {
+  skilltab.style.display = "none";
+}
+function hideeducation() {
+  educationtab.style.display = "none";
+}
+function hideexperience() {
+  experiencetab.style.display = "none";
+}
+function hidetools() {
+  toolstab.style.display = "none";
+}
+function hidequalities() {
+  qualitiestab.style.display = "none";
+}
+function hidefiles() {
+  filestab.style.display = "none";
 }
 
 window.onscroll = function () {
