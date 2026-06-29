7035f66baaf8c23811eb95d8ea36f29c01bbb818
Anand-004 <anandsince2004@gmail.com>
2026-06-29 18:17:02 +0530

    finished projeects page framework

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ var experiencetab = document.getElementById("experience");
 var toolstab = document.getElementById("tools");
 var qualitiestab = document.getElementById("qualities");
 var filestab = document.getElementById("files");
+var checkboxbutton =document.getElementById("procertswitcher");
+var certificatetab =document.getElementById("certificate-case");
+var projecttab = document.getElementById("project-case");
+var proheading = document.getElementById("proheading");
+var certheading = document.getElementById("certheading");
 
 function showoverlay() {
   overlay.style.display = "flex";
@@ -63,6 +68,44 @@ function construction() {
 function submission() {
   alert("Message Sent Successfully ..!");
 }
+
+function showcertifications(){
+    projecttab.style.display="none";
+    certificatetab.style.display="flex";
+
+    // proheading.style.fontSize="26px";
+    proheading.style.color="gray";
+    proheading.style.transform="scale(0.83)";
+
+    // certheading.style.fontSize="36px";
+    certheading.style.color="black";
+    certheading.style.transform="scale(1.2)";
+}
+
+function showprojects(){
+    certificatetab.style.display="none";
+    projecttab.style.display="flex";
+
+    // proheading.style.fontSize="36px";
+    proheading.style.color="black";
+    proheading.style.transform="scale(1.2)";
+
+    // certheading.style.fontSize="26px";
+    certheading.style.color="gray";
+    certheading.style.transform="scale(0.83)";
+}
+
+function checkbox() {
+  if(checkboxbutton.checked){
+    // alert("checked");
+    showcertifications();
+  }
+  else{
+    // alert("unchecked");
+    showprojects();
+  }
+}
+
 
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
